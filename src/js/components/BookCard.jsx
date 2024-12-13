8809aaca29ab892e45460b018b0abb952081bfc8
Shane B <dotfortun@users.noreply.github.com>
2024-12-13 22:59:58 +0000

    I forgot to commit again.

--- src/js/components/BookCard.jsx
+++ src/js/components/BookCard.jsx
@@ -1,34 +1,43 @@
-const BookCard = ({ book }) => {
-  // Style is an object in React, and not a string.
-  const style = { width: "45%" };
+import "./BookCard.css";
 
+const BookCard = ({ book, onToggleRead }) => {
   return (
-    <div className="card mb-3" style={style}>
-      {/* Self-closing tags MUST have the trailing forward slash. */}
-      <img className="card-img-top" src={book.cover} alt="" />
-      <div className="card-body">
-        <h5 className="card-title">{book.title}</h5>
-        <h6 className="card-subtitle mb-2 text-body-secondary">
-          {book.author}
-        </h6>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">
-            <strong>Pages:</strong> {book.num_pages}
-          </li>
-          <li className="list-group-item">
-            <strong>Published:</strong> {book.year_published}
-          </li>
-          <li className="list-group-item">
-            <strong>ISBN-10:</strong> {book.isbn10 ? book.isbn10 : "N/A"}
-          </li>
-          <li className="list-group-item">
-            <strong>ISBN-13:</strong> {book.isbn13 ? book.isbn13 : "N/A"}
-          </li>
-          <li className="list-group-item">
-            <strong>Is this book awesome:</strong>
-            {book.is_awesome ? " Yes" : " No"}.
-          </li>
-        </ul>
+    <div class="card mb-3">
+      <div class="row g-0">
+        <div class="col-4">
+          <img
+            src={book.cover}
+            class="img-fluid rounded-start book-cover"
+            alt="..."
+          />
+        </div>
+        <div class="col-8">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 className="card-subtitle">{book.author}</h6>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">
+                <strong>Pages:</strong> {book.num_pages}
+              </li>
+              <li class="list-group-item">
+                <strong>Published:</strong> {book.year_published}
+              </li>
+              <li class="list-group-item">
+                <strong>Is this book awesome:</strong> Yes.
+              </li>
+              {book.have_read ? (
+                <button className="btn btn-success" onClick={onToggleRead}>
+                  I've read this!
+                </button>
+              ) : (
+                <button className="btn btn-warning" onClick={onToggleRead}>
+                  I have not read this (yet.) :(
+                </button>
+              )}
+              <li class="list-group-item"></li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,51 +1,82 @@
 //include images into your bundle
+import { useState } from "react";
+
 import { Container, Row, Col } from "./Layout";
-import zero_thoughts from "../../img/zero_thought.jpg";
-import Timer from "./Timer";
-import { useState, useEffect } from "react";
-import VolumeSlider from "./VolumeSlider";
+import BookCard from "./BookCard";
 
 //create your first component
 const Home = () => {
-  // We need some way to store a value that when it changes,
-  // React cares about it.
-  const [value, setValue] = useState(0);
+  const [library, setLibrary] = useState([
+    {
+      title: "Something Wicked This Way Comes",
+      author: "Ray Bradbury",
+      cover: "https://pictures.abebooks.com/isbn/9780553136951-us.jpg",
+      num_pages: 293,
+      year_published: 1997,
+      isbn13: "978-0-380-72940-1",
+      isbn10: "0-380-72940-7",
+      is_awesome: true,
+      have_read: false,
+    },
+    {
+      title: "One Hundred Years of Solitude",
+      author: "Gabriel Garcia Marquez",
+      cover: "https://m.media-amazon.com/images/I/81dy4cfPGuL._SY522_.jpg",
+      num_pages: 383,
+      year_published: 1970,
+      isbn13: null,
+      isbn10: "0-380-01503-X",
+      is_awesome: true,
+      have_read: false,
+    },
+    {
+      title: "Snow Crash",
+      author: "Neal Stephenson",
+      cover:
+        "https://i5.walmartimages.com/seo/Snow-Crash-Hardcover-9780613361620_f11eea3c-5e60-4a1b-936b-67c9c4455e27_1.0a9c061d4600a35fb739ad85e9e9aa06.jpeg",
+      num_pages: 470,
+      year_published: 1992,
+      isbn13: "978-061336162",
+      isbn10: null,
+      is_awesome: true,
+      have_read: false,
+    },
+    {
+      title: "The Ultimate Hitchiker's Guide To The Galaxy",
+      author: "Douglas Adams",
+      cover:
+        "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1404613595i/13.jpg",
+      num_pages: 815,
+      year_published: 2002,
+      isbn13: "978-0-645-45374-7",
+      isbn10: null,
+      is_awesome: true,
+      have_read: false,
+    },
+  ]);
 
-  // We need some way to start a useInterval safely
-  // e.g. It needs to start once, instead of once every
-  // time the component re-renders.
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setValue((prev) => prev + 1);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  useEffect(() => {
-    console.log(`Value is: ${value}`);
-    // if (value == 30) {
-    //   alert("Ding!");
-    // }
-  }, [value]);
-
-  // We need to convert a number without leading zeros
-  // into a string of the correct length with leading zeros.
-  // e.g. 1000 ==> "01000"
+  const toggleRead = (idx) => {
+    let oldBook = library[idx];
+    oldBook.have_read = !oldBook.have_read;
+    setLibrary(library.toSpliced(idx, 1, oldBook));
+  };
 
   return (
     <Container breakpoint="md">
       <Row>
         <Col>
-          <Timer value={value.toString().padStart(5, "0")} />
+          <div className="d-flex flex-column align-items-center gap-3 mt-2">
+            {library.map((book, idx) => (
+              <BookCard
+                book={book}
+                key={idx}
+                onToggleRead={() => toggleRead(idx)}
+              />
+            ))}
+          </div>
         </Col>
       </Row>
       <div className="mt-2"></div>
-      <Row>
-        <Col>
-          <VolumeSlider />
-        </Col>
-      </Row>
     </Container>
   );
 };
